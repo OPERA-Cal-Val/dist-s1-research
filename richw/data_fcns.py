12b0faec29fc479fe5a2f222076c4d4340de78ba
Richard D West <richw@aurora.jpl.nasa.gov>
2024-04-09 09:40:41 -0700

    dev: added richw with initial files

---
+++ richw/data_fcns.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+
+import pandas as pd
+import geopandas as gpd
+
+# Functions to access/manipulate OPERA data
+
+def get_index(data: list[str], val: str) -> int:
+    try:
+        ind = data.index(val)
+    except ValueError:
+        ind = len(data)
+    return ind
+
+def get_first_change(labeled_ts):
+    get_index_p = lambda val: get_index(labeled_ts, val)
+    indices = list(map(get_index_p, ['VLmin', 'VLmaj', 'OCmin', 'OCmaj']))
+    ind = min(indices)
+    ind = ind if ind < len(labeled_ts) else -1
+    return ind
+
+def get_last_noChange(labeled_ts, change_ind):
+    if change_ind == -1:
+        return -1
+    labeled_ts_r = (labeled_ts[:change_ind][::-1])
+    ind_nc_r = get_index(labeled_ts_r, 'noChange')
+    ind_nc = change_ind - ind_nc_r - 1
+    return ind_nc
+
+def get_first_change_from_row(row):
+    change_label = row['overallLabel']
+    if change_label in ['VLmin', 'VLmaj', 'OCmaj', 'OCmin']:
+        dates_str = row.keys().tolist()[5:]
+        dates = pd.to_datetime(dates_str)
+        
+        labeled_ts = list(row)[5:]
+        ind_c = get_first_change(labeled_ts)
+        change_date = dates[ind_c]
+    else:
+        change_date = pd.NaT
+    return change_date
+
+def get_last_obs_date_before_change(row):
+    change_label = row['overallLabel']
+    if change_label in ['VLmin', 'VLmaj', 'OCmaj', 'OCmin']:
+        dates_str = row.keys().tolist()[5:]
+        dates = pd.to_datetime(dates_str)
+        
+        labeled_ts = list(row)[5:]
+        ind_c = get_first_change(labeled_ts)
+        ind_nc = get_last_noChange(labeled_ts, ind_c)
+        last_obs_date = dates[ind_nc] if ind_nc > -1 else pd.NaT
+    else:
+        last_obs_date = pd.NaT
+
+    return last_obs_date
+
---
+++ richw/try1.py
@@ -0,0 +1,58 @@
+#! /u/aurora-r0/richw/pkgs/miniforge3/envs/dist-s1/bin/python -i
+
+import os
+import geopandas as gpd
+from rasterio.crs import CRS
+import matplotlib.pyplot as plt
+from shapely.geometry import Point
+import pandas as pd
+from dem_stitcher.geojson_io import read_geojson_gzip
+from dem_stitcher.geojson_io import to_geojson_gzip
+from shapely.geometry import Polygon, shape
+import asf_search as asf
+import concurrent.futures
+from tqdm import tqdm
+import data_fcns
+
+filename_burst_id = 'opera_burst_ids.geojson.zip'
+url_burst_id = 'https://github.com/opera-adt/burst_db/releases/download/v0.3.1/burst-id-geometries-simple-0.3.1.geojson.zip'
+
+# Check for burst_id database and pull if needed
+if os.path.isfile(filename_burst_id):
+  print(f"Using burst_id database file: {filename_burst_id}")
+else:
+  print(f"Loading burst_id database from {url_burst_id}")
+  df = gpd.read_file(url_burst_id)
+  print(f"Writing burst_id database file to: {filename_burst_id}")
+  to_geojson_gzip(df, filename_burst_id)
+
+# Read disturbance data
+df_dist_hls_val = pd.read_csv('https://raw.githubusercontent.com/OPERA-Cal-Val/DIST-Validation/main/referenceTimeSeries_last.csv')
+
+# Testing
+IND_T = 2
+row_data = list(df_dist_hls_val.iloc[IND_T])
+ts_labeled = row_data[5:]
+ind_c = data_fcns.get_first_change(ts_labeled)
+ind_nc = data_fcns.get_last_noChange(ts_labeled, ind_c)
+
+change_times = df_dist_hls_val.apply(data_fcns.get_first_change_from_row,
+  axis=1)
+last_obs_times = df_dist_hls_val.apply(
+  data_fcns.get_last_obs_date_before_change, axis=1)
+change_labels = df_dist_hls_val.overallLabel[:10]
+
+df_sites = gpd.GeoDataFrame({'site_id': df_dist_hls_val.ID,
+  'change_label': df_dist_hls_val.overallLabel,
+  'change_time': change_times,
+  'last_observation_time': last_obs_times},
+  geometry=gpd.points_from_xy(df_dist_hls_val.Long,
+  df_dist_hls_val.Lat),
+  crs=CRS.from_epsg(4326))
+
+# Plot the test site locations
+fig,ax = plt.subplots()
+df_sites.plot(ax=ax)
+fig.savefig('sites.png',dpi=300,bbox_inches="tight")
+
+df_sites.to_file('dist_hls_val_sites.geojson', driver='GeoJSON')
