d0a87bbca27dd7443aaa8d7f438f92591a3a0d83
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2024-08-14 15:12:23 -0700

    fix tqdm and format py

--- marshak/10_ad_hoc_data_generation/run_events.py
+++ marshak/10_ad_hoc_data_generation/run_events.py
@@ -1,30 +1,32 @@
 from pathlib import Path
 
 import click
-import geopandas as gpd
 import papermill as pm
 from tqdm import tqdm
 
 
-@click.option('--event_name',
-              required=True,
-              type=str,
-              multiple=True,
-              help='Provide names of events')
+@click.option(
+    "--event_name",
+    required=True,
+    type=str,
+    multiple=True,
+    help="Provide names of events",
+)
 @click.command()
 def main(event_name: list):
-
     event_names = list(event_name)
 
-    in_nbs = ['1__DIST-HLS.ipynb',
-              '2__RTC-S1.ipynb',
-              '3__Validation_Data.ipynb',
-              '4__Water_Mask.ipynb']
+    in_nbs = [
+        "1__DIST-HLS.ipynb",
+        "2__RTC-S1.ipynb",
+        "3__Validation_Data.ipynb",
+        "4__Water_Mask.ipynb",
+    ]
 
     ipynb_out_dir = Path("out_notebooks")
     ipynb_out_dir.mkdir(exist_ok=True, parents=True)
 
-    for event_name in enumerate(event_names, desc='events'):
+    for event_name in tqdm(event_names, desc="events"):
         print(event_name)
         out_site_nb_dir = ipynb_out_dir / event_name
         out_site_nb_dir.mkdir(exist_ok=True, parents=True)
