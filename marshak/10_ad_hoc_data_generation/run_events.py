d248a99d9900e76a96a426707fe8dfc3de8ab8c8
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2024-08-14 13:07:37 -0700

    rename and add papermill

---
+++ marshak/10_ad_hoc_data_generation/run_events.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import click
+import geopandas as gpd
+import papermill as pm
+from tqdm import tqdm
+
+
+@click.option('--event_name',
+              required=True,
+              type=str,
+              multiple=True,
+              help='Provide names of events')
+@click.command()
+def main(event_name: list):
+    print(event_name)
+
+    event_names = list(event_name)
+
+    in_nbs = ['1__DIST-HLS.ipynb',
+              '2__RTC-S1.ipynb',
+              '3__Validation_Data.ipynb',
+              '4__Water_Mask.ipynb']
+
+    ipynb_out_dir = Path("out_notebooks")
+    ipynb_out_dir.mkdir(exist_ok=True, parents=True)
+
+    for event_name in event_names:
+        out_site_nb_dir = ipynb_out_dir / event_name
+        out_site_nb_dir.mkdir(exist_ok=True, parents=True)
+        for in_nb in in_nbs:
+            pm.execute_notebook(
+                in_nb,
+                output_path=out_site_nb_dir / in_nb,
+                parameters=dict(EVENT_NAME=event_name),
+            )
+
+
+if __name__ == "__main__":
+    main()
